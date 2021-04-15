355eed3a8a397c1de531c3771a15a4b3aadb44a3
cambateman <cambateman604@gmail.com>
2021-04-14 18:23:01 -0700

    fixed routing for login/logout

--- routes/users.js
+++ routes/users.js
@@ -30,7 +30,7 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
+    const redirectUrl = req.session.returnTo || '/athletes';
     delete req.session.returnTo;
     res.redirect(redirectUrl);
 })
@@ -38,7 +38,7 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 router.get('/logout', (req, res) => {
     req.logout();
     req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
+    res.redirect('/athletes');
 })
 
 module.exports = router;
